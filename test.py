72e605aef3c06491cd000cad3e8586b5b808d184
forever-notice <royee_peng@outlook.com>
2025-10-19 15:20:18 +0800

    反转矩阵

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-# leetcode 0724 
+# Leetcode 048
+from inspect import markcoroutinefunction
+
+
+class Solution:
+    def rotate(self,matrix:list[list[int]]) -> None:
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(n):
+                matrix[i][j], matrix[n-i-1][j] = matrix[n-i-1][j],matrix[i][j]
+
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
